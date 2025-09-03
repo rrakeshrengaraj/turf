58cfe65ac5bceafa46f509b5180e49fea47fff8b
Rakesh <rrakeshrengaraj@gmail.com>
2025-09-03 07:12:44 +0530

    Update GalleryPage.jsx

--- src/Pages/GalleryPage.jsx
+++ src/Pages/GalleryPage.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const images = [
-  "src/assets/g1.jpg",
-  "src/assets/g2.jpg",
-  "src/assets/g3.jpg",
-  "src/assets/g4.jpg",
-  "src/assets/g5.jpg",
-  "src/assets/g6.jpg",
-  "src/assets/g7.jpg",
-];
+import g1 from "../assets/g1.jpg";
+import g2 from "../assets/g2.jpg";
+import g3 from "../assets/g3.jpg";
+import g4 from "../assets/g4.jpg";
+import g5 from "../assets/g5.jpg";
+import g6 from "../assets/g6.jpg";
+import g7 from "../assets/g7.jpg";
+
+const images = [g1, g2, g3, g4, g5, g6, g7];
+
 
 export default function GalleryPage() {
   const slides = [images[images.length - 1], ...images, images[0]];
